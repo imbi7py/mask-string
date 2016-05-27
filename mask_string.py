222e056f3b877e1e446e1ffaa34c1eadfe677080
Michael Kennedy <mikeckennedy@gmail.com>
2016-05-27 11:38:51 +0200

    more error handling and edge cases.

--- mask_string.py
+++ mask_string.py
@@ -2,13 +2,18 @@ import sys
 
 
 def mask_string(string, start=None, stop=None, mask='****', match_length=True):
+    if string is None:
+        return None
+    
     length = len(string)
     if start is None:
         start = 0
     if stop is None:
         stop = max(0, length - 1)
-    start %= length
-    stop %= length
+
+    if length:
+        start %= length
+        stop %= length
 
     if match_length and (not mask or len(mask) != length):
         mask = mask[0] * length
--- test.py
+++ test.py
@@ -4,10 +4,12 @@ import mask_string
 
 class TestMaskString(unittest.TestCase):
     def testMask(self):
-        self.assertEqual(mask_string('Hello world!', 2, 7), 'He****rld!')
-        self.assertEqual(mask_string('Hello world!', 2, -1), 'He****')
-        self.assertEqual(mask_string('Hello world!', 2, 7, mask='...'), 'He...rld!')
-        self.assertEqual(mask_string('Hello world!', 0, 7, mask='...'), '...rld!')
+        self.assertEqual(mask_string('Hello world!', 2, 7, match_length=False), 'He****rld!')
+        self.assertEqual(mask_string('Hello world!', 2, -1, match_length=False), 'He****')
+        self.assertEqual(mask_string('Hello world!', 2, 7, mask='...', match_length=False), 'He...rld!')
+        self.assertEqual(mask_string('Hello world!', 0, 7, mask='...', match_length=False), '...rld!')
+        self.assertEqual(mask_string(''), '')
+        self.assertEqual(mask_string(None), None)
 
 
 if __name__ == '__main__':
