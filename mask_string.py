376a0c02296b3416917f9dc2b015023b4d24837e
Michael Kennedy <mikeckennedy@gmail.com>
2016-05-27 11:36:12 +0200

    Adds default values to start, stop and ability to match length with mask.

--- mask_string.py
+++ mask_string.py
@@ -1,9 +1,18 @@
 import sys
 
-def mask_string(string, start, stop, mask='****'):
-	length = len(string)
-	start = start % length
-	stop = stop % length
-	return string[:start] + mask + string[stop+1:]
+
+def mask_string(string, start=None, stop=None, mask='****', match_length=True):
+    length = len(string)
+    if start is None:
+        start = 0
+    if stop is None:
+        stop = max(0, length - 1)
+    start %= length
+    stop %= length
+
+    if match_length and (not mask or len(mask) != length):
+        mask = mask[0] * length
+
+    return string[:start] + mask + string[stop+1:]
 
 sys.modules[__name__] = mask_string
